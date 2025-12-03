6c5e4cb81e28025759cfe84f6e07b8d7112f56f6
anntrofim-debug <ann.trofim@gmail.com>
2025-12-03 11:45:58 +0100

    Add login component

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,12 @@
+import styles from './login.module.css'
+
+const Login = () => {
+    return (
+      <section className={styles.container}>
+        <h1>Julemandens Værksted</h1>
+        
+      </section>
+    );
+}
+
+export default Login
