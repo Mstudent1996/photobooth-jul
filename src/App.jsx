d974288fd0bbc8370e73ab5db3d476a66b192038
anntrofim-debug <ann.trofim@gmail.com>
2025-12-04 11:46:12 +0100

    Add login pop-up component
    Closes #11

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 import WebcamComponent from "./components/webcam/webcam";
+import LoginModal from "./components/login/Login";
+import { useState } from "react";
 
 export default function App() {
-  return <WebcamComponent />;
+
+  const [showModal, setShowModal] = useState(false)
+
+  return (
+    <>  
+    <WebcamComponent />;
+  
+  <div>
+    <button className="btn btn-primary" onClick={() => setShowModal(true)}>
+      Open Login Modal
+    </button>
+    <LoginModal show={showModal} onClose={() => setShowModal(false)} />
+  </div>;
+  </>
+
+)
 }
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,12 +1,56 @@
 import styles from './login.module.css'
 
-const Login = () => {
+const LoginModal = ({show, onClose}) => {
+
+        const handleLogin = (e) => {
+            e.preventDefault()
+
+            const email = e.target.email.value
+            const password = e.target.password.value
+            console.log("Email:", email, "Password:", password);
+            onclose()
+        }
+
+        if(!show) return null
+
     return (
       <section className={styles.container}>
-        <h1>Julemandens Værksted</h1>
-        
+        <h1 className={styles.title}>Julemandens Værksted</h1>
+
+        <button className={styles.closeBtn} type="button" onClick={onClose}>
+          &times;
+        </button>
+
+        <form onSubmit={handleLogin}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label"></label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label"></label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-success w-100">
+            Login
+          </button>
+        </form>
+
       </section>
     );
 }
 
-export default Login
+export default LoginModal
