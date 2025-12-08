5bf8d19aba07f04553795005325bd33707d96a16
anntrofim-debug <ann.trofim@gmail.com>
2025-12-08 13:08:57 +0100

    Add correct class

--- src/components/frontpage/FrontpageComp.jsx
+++ src/components/frontpage/FrontpageComp.jsx
@@ -36,7 +36,7 @@ const FrontpageComp = () => {
           </div>
         </div>
 
-        <div className={styles.btns}>
+        <div className={styles.buttons}>
             
           <Link to="/gallery">
             <button className={styles.button}>Se nissernes billedbog</button>
