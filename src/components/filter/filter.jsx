9f98cfb609fab7e1594dcbbaa3c7e5cc998e7525
Mstudent1996 <1000041083@mcdmstudent.dk>
2025-12-09 09:05:28 +0100

    Filter størrelse ændret

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,7 +1,6 @@
 import styles from "./filter.module.css";
 import filterImg1 from "/src/assets/img.png";
 import filterImg2 from "/src/assets/img2.png";
-import filterImg3 from "/src/assets/img.png";
 import filterImg4 from "/src/assets/grinchBorder_640x480.png";
 
 export default function FilterComponent({ onSelectFilter }) {
@@ -19,14 +18,6 @@ export default function FilterComponent({ onSelectFilter }) {
         <img
           src={filterImg2}
           onClick={() => onSelectFilter(filterImg2)}
-          alt="Filter"
-        />
-      </div>
-
-      <div className={styles.filter}>
-        <img
-          src={filterImg3}
-          onClick={() => onSelectFilter(filterImg3)}
           alt="Filter"
         />
       </div>
